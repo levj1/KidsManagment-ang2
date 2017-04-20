e56a0b858f779e3418c163455f4268f2c3735958
James Leveille <levj1@yahoo.com>
2017-04-19 23:08:23 -0400

    create project

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+
+import{ Component } from '@angular/core';
+
+@Component({
+    selector: 'kpm-app',
+    template: `
+        <p>You love your kids, we want to keep them safe</p>
+    `
+})
+export class AppComponent{
+    pageTitle: string = "Kids Management App"
+}
